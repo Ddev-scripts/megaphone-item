482288d50517ed5ebdab937538e34f226324ded2
Nema <petermannfelipe@gmail.com>
2023-03-17 21:25:02 -0300

    for esx and qbcore

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,15 @@
-ESX.RegisterUsableItem('megaphone', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer then
+if Config.Framework == 'qb-core' then
+    QBCore = exports['qb-core']:GetCoreObject()
+    QBCore.Functions.CreateUseableItem('megaphone', function(source)
         TriggerClientEvent('megaphone:use', source)
-    end
-end)
+    end)
+elseif Config.Framework == 'esx' then
+    ESX = exports['es_extended']:getSharedObject()
+
+    ESX.RegisterUsableItem('megaphone', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer then
+            TriggerClientEvent('megaphone:use', source)
+        end
+    end)
+end
